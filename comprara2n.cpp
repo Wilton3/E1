cf39b3e02298b18f48998698513d1d56ee557ae4
Wilton3 <julianaorejuela26@gmail.com>
2022-09-30 07:25:09 -0500

    E1

---
+++ comprara2n.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y;
+	cout<<"ingrese x :"; cin>>x;
+	cout<<"ingrese y :"; cin>>y;
+
+	
+	  if (x==y){
+		  cout<<"El valor de x= "<<x<<"es igual a y= "<<y<<endl;
+
+
+
+
+
+	  }else{
+		  if(x<y){
+			  cout<<"El valor de x= "<<x<<"Es menor a y= "<<y<<endl;
+
+			    
+
+
+			 }else{
+				 cout<<"El valor de y= "<<y<<"Es menor a x= "<<x<<endl;
+
+
+
+               }
+
+
+
+		  }
+
+
+
+
+
+
+
+	  
+
+
+	
+
+	return 0;
+
+
+
+
+
+
+}
